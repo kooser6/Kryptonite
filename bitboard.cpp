0e4423e90f637a75c0195f95059e49d08552c132
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-03-24 01:15:01 -0400

    Include bitboard header file.

--- bitboard.cpp
+++ bitboard.cpp
@@ -2,13 +2,5 @@
 #include <string>
 #include <bitset>
 #include <cassert>
+#include <bitboard.h>
 
-class BitBoard {
-    private:
-        std::bitset<256> board;
-    public:
-        void shiftN(void) {
-            // Shifts bitboard north.
-            board = (board << 16);
-        };
-};
