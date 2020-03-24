b51ac8a20b26cdad3b6bc7a83c70d8b8f7a480a4
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-03-24 00:58:34 -0400

    Add some code.

--- bitboard.cpp
+++ bitboard.cpp
@@ -2,3 +2,13 @@
 #include <string>
 #include <bitset>
 #include <cassert>
+
+class BitBoard {
+    private:
+        std::bitset<256> board;
+    public:
+        void shiftN(void) {
+            // Shifts bitboard north.
+            board = (board << 16);
+        };
+};
